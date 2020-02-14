83d2936da9d3e828228a29a38ceed174d37761a2
Maksym Shokin <maksym.sh@upplabs.com>
2020-02-15 01:12:41 +0200

    'more-stack-migration'

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -6,13 +6,23 @@ import { Ionicons } from '@expo/vector-icons';
 import { useDispatch } from 'react-redux';
 
 import ProductsOverviewScreen, {
-  screenOptions
+  screenOptions as productsOverviewScreenOptions
 } from '../screens/shop/ProductsOverviewScreen';
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
-import CartScreen from '../screens/shop/CartScreen';
-import OrdersScreen from '../screens/shop/OrdersScreen';
-import UserProductsScreen from '../screens/user/UserProductsScreen';
-import EditProductScreen from '../screens/user/EditProductScreen';
+import ProductDetailScreen, {
+  screenOptions as productDetailScreenOptions
+} from '../screens/shop/ProductDetailScreen';
+import CartScreen, {
+  screenOptions as cartScreenOptions
+} from '../screens/shop/CartScreen';
+import OrdersScreen, {
+  screenOptions as ordersScreenOptions
+} from '../screens/shop/OrdersScreen';
+import UserProductsScreen, {
+  screenOptions as userProductsScreenOptions
+} from '../screens/user/UserProductsScreen';
+import EditProductScreen, {
+  screenOptions as editProductScreenOptions
+} from '../screens/user/EditProductScreen';
 import AuthScreen from '../screens/user/AuthScreen';
 import StartupScreen from '../screens/StartupScreen';
 import Colors from '../constants/Colors';
@@ -35,17 +45,22 @@ const ProductsStackNavigator = createStackNavigator();
 
 export const ProductsNavigator = () => {
   return (
-    <ProductsStackNavigator.Navigator screenOptions={defaultNavOptions} >
+    <ProductsStackNavigator.Navigator screenOptions={defaultNavOptions}>
       <ProductsStackNavigator.Screen
         name="ProductsOverview"
         component={ProductsOverviewScreen}
-        options={screenOptions}
+        options={productsOverviewScreenOptions}
       />
       <ProductsStackNavigator.Screen
         name="ProductDetail"
         component={ProductDetailScreen}
+        options={productDetailScreenOptions}
       />
-      <ProductsStackNavigator.Screen name="Cart" component={CartScreen} />
+      <ProductsStackNavigator.Screen
+        name="Cart"
+        component={CartScreen}
+        options={cartScreenOptions}
+      />
     </ProductsStackNavigator.Navigator>
   );
 };
@@ -70,6 +85,20 @@ export const ProductsNavigator = () => {
 //   }
 // );
 
+const OrdersStackNavigator = createStackNavigator();
+
+export const OrdersNavigator = () => {
+  return (
+    <OrdersStackNavigator.Navigator screenOptions={defaultNavOptions}>
+      <OrdersStackNavigator.Screen
+        name="Orders"
+        component={OrdersScreen}
+        options={ordersScreenOptions}
+      />
+    </OrdersStackNavigator.Navigator>
+  );
+};
+
 // const OrdersNavigator = createStackNavigator(
 //   {
 //     Orders: OrdersScreen
@@ -87,6 +116,25 @@ export const ProductsNavigator = () => {
 //     defaultNavigationOptions: defaultNavOptions
 //   }
 // );
+
+const AdminStackNavigator = createStackNavigator();
+
+export const AdminNavigator = () => {
+  return (
+    <AdminStackNavigator.Navigator screenOptions={defaultNavOptions}>
+      <AdminStackNavigator.Screen
+        name="UserProducts"
+        component={UserProductsScreen}
+        options={userProductsScreenOptions}
+      />
+      <AdminStackNavigator.Screen
+        name="EditProduct"
+        component={EditProductScreen}
+        options={editProductScreenOptions}
+      />
+    </AdminStackNavigator.Navigator>
+  );
+};
 
 // const AdminNavigator = createStackNavigator(
 //   {
